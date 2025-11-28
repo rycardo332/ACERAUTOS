86da8cfb385f290a727cd3fbf12b00eb10719167
Angie Yuliana Pedraza Barrera <angiey_pedrazaq@soy.sena.edu.co>
2025-11-27 21:24:19 -0500

    se agrego el model de Ventas y Proveedores

--- grupo6/app/models.py
+++ grupo6/app/models.py
@@ -15,3 +15,37 @@ class Factura(models.Model):
         verbose_name = "Factura"
         verbose_name_plural = "Facturas"
         db_table = "Factura"
+        
+class Ventas(models.Model):
+    fecha = models.DateField()
+    total = models.DecimalField(max_digits=10,decimal_places=2) 
+    documento = models.CharField(max_length=10)
+    cliente = models.ForeignKey('Cliente',on_delete=models.CASCADE)
+    salida = models.ForeignKey('Salida_vehiculo',on_delete=models.CASCADE)   
+    productos = models.ForeignKey('Producto',on_delete=models.CASCADE) 
+    usuario = models.ForeignKey('User',on_delete=models.CASCADE)     
+    
+    
+    def __str__(self):
+        return self.fecha , self.cliente
+    class Meta:
+        verbose_name = "Venta"
+        verbose_name_plural = "Ventas"
+        db_table = "Venta"
+        
+
+class Proveedores(models.Model):
+    nombre = models.CharField(max_length=50)
+    documento = models.CharField(max_length=10)
+    telefono = models.CharField(max_length=15)
+    email = models.EmailField()
+    direccion = models.CharField(max_length=100)
+    estado = models.BooleanField(default=True)
+    mercancia = models.ForeignKey('Producto',on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.nombre
+    class Meta:
+        verbose_name = "Proveedor"
+        verbose_name_plural = "Proveedores"
+        db_table = "Proveedor"
