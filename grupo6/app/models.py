6a48ee214d0cffdf1df0fe3d4bb874674a84e088
Angie Yuliana Pedraza Barrera <angiey_pedrazaq@soy.sena.edu.co>
2025-11-27 20:03:09 -0500

    Se agrego el model Factura

--- grupo6/app/models.py
+++ grupo6/app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Factura(models.Model):
+    fecha = models.DateField()
+    subtotal = models.DecimalField(max_digits=10,decimal_places=2)
+    iva = models.DecimalField(max_digits=10,decimal_places=2)
+    total = models.DecimalField(max_digits=10,decimal_places=2)
+    venta = models.ForeignKey('Venta',on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.total , self.venta.fecha
+    class Meta:
+        verbose_name = "Factura"
+        verbose_name_plural = "Facturas"
+        db_table = "Factura"
